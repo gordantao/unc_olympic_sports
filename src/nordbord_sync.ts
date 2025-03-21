f2266eb515444acf30976a9a9d1f8c8d64edc13f
GORDAN TAO <taogordan@gmail.com>
2025-03-21 12:43:35 -0400

    Resolved infinite while loop.

--- src/nordbord_sync.ts
+++ src/nordbord_sync.ts
@@ -9,14 +9,14 @@ type ValdTestResults = {
   tests: Array<ValdTest>;
 }
 
-const endpoint: string = 'https://prd-use-api-externalnordbord.valdperformance.com';
-const team_id: string = '5b4690e7-25c4-4b79-927f-aec642e3c53e';
-const start_date: string = '2000-01-01T00:00:00.000Z'
+const ENDPOINT: string = 'https://prd-use-api-externalnordbord.valdperformance.com';
+const TEAM_ID: string = '5b4690e7-25c4-4b79-927f-aec642e3c53e';
+const START_DATE: Date = new Date('1900-01-01T00:00:00.000Z');
 const EXPORT_DATA: boolean = false;
 
-async function get_data(date: string): Promise<Response> {
+async function get_data(date: Date): Promise<Response> {
   const response = await fetch(
-    endpoint + `/tests/v2?TenantId=${team_id}&ModifiedFromUtc=${date}`,
+    ENDPOINT + `/tests/v2?TenantId=${TEAM_ID}&ModifiedFromUtc=${date.toISOString()}`,
     {
       method: 'GET',
       headers: {
@@ -27,22 +27,20 @@ async function get_data(date: string): Promise<Response> {
   return response
 }
 
-async function get_nordboard() {
-  var response: Response = await get_data(start_date);
+async function get_nordbord() {
+  var response: Response = await get_data(START_DATE);
   var tests: Array<ValdTest> = [];
-  var last_date: string = start_date;
-  var current_date: string;
+  var lastDate: Date = START_DATE;
+  var currentDate: string;
 
   while ((response.status != 204) && (response.status == 200)) {
-    console.log("Status code: " + response.status);
     var data: ValdTestResults = await response.json()
     data.tests.forEach(element => {
       tests.push(element);
     });
-    last_date = data.tests[data.tests.length - 1].modifiedDateUtc;
-    console.log("Last date: " + last_date);
-    console.log("===========");
-    response = await get_data(last_date);
+    lastDate = new Date(data.tests[data.tests.length - 1].modifiedDateUtc);
+    lastDate.setMilliseconds(lastDate.getMilliseconds() + 1);
+    response = await get_data(lastDate);
   }
   if (response.status == 204) {
     return tests;
@@ -51,4 +49,14 @@ async function get_nordboard() {
   }
 }
 
-get_nordboard();
+if (EXPORT_DATA) {
+  get_nordbord().then((tests) => {
+    console.log(`loaded ${tests.length} rows of data`)
+    saveJsonToCsv(tests, 'nordbord.csv')
+  });
+} else {
+  get_nordbord().then((tests) => {
+    console.log(`loaded ${tests.length} rows of data`)
+    saveJsonToCsv(tests, 'nordbord.csv')
+  });
+}
